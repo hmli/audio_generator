00c86994121f126dcd38ae0fd99a0422bef1f339
lihaoming <lihaoming@mintegral.com>
2019-06-30 17:45:20 +0800

    remove tests

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,12 @@ import (
 	"audio/internal/texter"
 	"flag"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	conf := flag.String("conf", "config.toml", "config file path")
+	file := flag.String("f", "text.txt", "text file path")
 	flag.Parse()
 	cfg, err := config.ParseConfig(*conf)
 	if err != nil {
@@ -18,9 +20,12 @@ func main() {
 	}
 	fmt.Printf("cfg:%+v\n", *cfg)
 	app := api.NewApp(cfg)
-	text := `
-	伴随着二次元文化而生的A站，从诞生之初就充斥着亚文化的气质：“ACFun”取意自“Anime Comic Fun”，以及“天下漫友是一家”，一小群屡遭误解、排挤的二次元文化爱好者聚拢在一起，用小圈层的自娱自乐反叛而不羁地解构主流文化	
-`
+
+	b, err := ioutil.ReadFile(*file)
+	if err != nil {
+		panic("open file err:"+err.Error())
+	}
+	text := string(b)
 	ss := texter.TextEveryLine([]byte(text))
 	param := api.Param{
 		Aue: api.AUE_MP3,
